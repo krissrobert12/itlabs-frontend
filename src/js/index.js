26046a9e03cbcb1e9fa0596eb30f44d85f79adf9
Gardner Kriss <krissrobert12@gmail.com>
2020-08-14 00:51:20 +0300

    Created slider and implemented responsive features; created courses section

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,75 @@
 import * as navView from './views/navView';
-import {domStrings} from './models/base';
+import {domStrings, resizeAwait} from './models/base';
+
+
+
+/**
+ * MAIN APP CONTROLLER
+ */
 
 document.querySelector(domStrings.navBtn).addEventListener('click', navView.toggleMenu);
+
+
+
+
+/**
+ * SLIDERS CONTROLLER
+ */
+
+// Functions
+const mountSliders = (sliders) => {
+    sliders.forEach(s => {
+        s.mount();
+    });
+};
+
+const updatePerPage = (nextSlider) => {
+    if (nextSlider) {
+        for(let i = 0; i < sliders.length; i++) {
+            sliders[i].options.perPage = 1;
+        }
+    } else {
+        for(let i = 0; i < sliders.length; i++) {
+            sliders[i].options.perPage = perPage[i];
+        }
+    }
+};
+
+const isViewportSmall = () => (window.innerWidth < 816);
+
+
+// Variable declarations
+let sliders = [];
+let perPage = [];
+let activeSlider = isViewportSmall();
+
+// Generate sliders
+const generate = () => {
+    sliders = [];
+    perPage = [];
+
+    sliders.push(new Splide('.splide', {
+        type   : 'loop',
+        perPage: 3,
+    }));
+    perPage.push(3);
+}
+
+generate();
+
+updatePerPage(activeSlider);
+mountSliders(sliders);
+
+
+const updateSliders = () => {
+    let nextSlider = isViewportSmall();
+    if (activeSlider != nextSlider) {
+        activeSlider = nextSlider;
+        sliders.forEach(slider => slider.destroy());
+        generate();
+        updatePerPage(nextSlider);
+        mountSliders(sliders);
+    }
+};
+
+resizeAwait(updateSliders);
