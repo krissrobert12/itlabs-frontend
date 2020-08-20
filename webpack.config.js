b0a08d80b024dcf312f324bcf0d5de8daecbfdb4
Gardner Kriss <krissrobert12@gmail.com>
2020-08-21 02:32:44 +0300

    Created entry configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
+    entry: {
+        main: './src/js/index.js',
+        cookie: './src/js/cookie-policy.js',
+        gdpr: './src/js/gdpr.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
+        filename: 'js/[name].js'
     },
     devServer: {
         contentBase: './dist'
@@ -13,15 +17,18 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: './src/index.html'
+            template: './src/index.html',
+            chunks: ['main']
         }),
         new HtmlWebpackPlugin({
             filename: 'cookie-policy.html',
-            template: './src/cookie-policy.html'
+            template: './src/cookie-policy.html',
+            chunks: ['cookie']
         }),
         new HtmlWebpackPlugin({
             filename: 'gdpr.html',
-            template: './src/gdpr.html'
+            template: './src/gdpr.html',
+            chunks: ['gdpr']
         })
     ],
     module: {
